26e3a785d189fc0b02a601f5176ec3f6213037ef
seyma <seymayetisen@gmail.com>
2018-06-03 01:12:15 +0300

    Add project files.

---
+++ deneme/Controllers/DenemeController.cs
@@ -0,0 +1,27 @@
+﻿using deneme.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deneme.Controllers
+{
+    public class DenemeController : Controller
+    {
+        // GET: Deneme
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public JsonResult IsLogin(string username,string sifre)
+        {
+            LoginYonetimDbContext aaaa = new LoginYonetimDbContext();
+            List<Login> lll = aaaa.login.Where(p=>p.username.Contains(username)).ToList();
+
+            return Json(lll, JsonRequestBehavior.AllowGet);
+            
+        }
+    }
+}
---
+++ deneme/Models/LoginYonetimDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace deneme.Models
+{
+    public class LoginYonetimDbContext : DbContext
+    {
+        public DbSet<Login> login { get; set; }
+        public LoginYonetimDbContext():base("Data Source=DESKTOP-SON6OA8;Initial Catalog=aaaa;Integrated Security=True")
+        {
+           
+            Database.SetInitializer<LoginYonetimDbContext>(new DbInitializer());
+
+            
+        }
+    }
+
+    public class DbInitializer : DropCreateDatabaseAlways<LoginYonetimDbContext>
+    {
+        protected override void Seed(LoginYonetimDbContext context)
+        {
+            Login lgn = new Login
+            {
+                username = "seyma",
+                passapord = "12345"
+            };
+            context.login.Add(lgn);
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
